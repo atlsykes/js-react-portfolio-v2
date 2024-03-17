46bb5e4f9952155aa7f3f6c97d6c1115f4e8360d
Justin Sykes <atlsykes1@att.net>
2024-03-17 13:57:05 -0400

    created footer card and styled it

---
+++ src/Components/card/footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./home-card.css";
+
+const footer = () => {
+  return (
+    <div className="footer-custom">
+      <hr></hr>
+      <h5>© 2024 - Justin Sykes</h5>
+    </div>
+  );
+};
+
+export default footer;
