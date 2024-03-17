d3edc0bd44dd352acd34a49e729579ebbd404268
Justin Sykes <atlsykes1@att.net>
2024-03-17 12:11:02 -0400

    added navbar, styled, and styled homecard body

--- src/Components/navbar/navbar.jsx
+++ src/Components/navbar/navbar.jsx
@@ -1,1 +1,48 @@
+import React from "react";
+import "./navbar.css";
+import JASlogo from "../../Assets/JAS-circ-logo.png";
 
+const navbar = () => {
+  return (
+    <div>
+      <nav class="navbar">
+        <div class="container-fluid">
+          <span className="press-start-2p-regular">
+            <ul class="navbar-nav">
+              <li>
+                <img
+                  src={JASlogo}
+                  alt="JAS circular logo"
+                  width="180"
+                  height="180"
+                ></img>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                  Home
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                  Bio
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                  Projects
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                  Recipes
+                </a>
+              </li>
+            </ul>
+          </span>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default navbar;
