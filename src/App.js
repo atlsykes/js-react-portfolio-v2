5bb33e6e4b432978766e4a2b67b9e212968e1d24
Justin Sykes <atlsykes1@att.net>
2024-04-11 16:50:23 -0400

    added page-not-found container, route, and styling to new page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Footer from "./Components/footer/footer";
 import Bio from "./Containers/bio/bio";
 import Portfolio from "./Containers/portfolio/portfolio";
 import Recipes from "./Containers/recipes/recipes";
+import PageNotFound from "./Containers/page-not-found/page-not-found";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 //Navbar has to be integrated at the card level due to it's vertical nature, but Footer can be integrated here
 
@@ -16,6 +17,7 @@ function App() {
           <Route path="/bio" element={<Bio />} />
           <Route path="/portfolio" element={<Portfolio />} />
           <Route path="/recipes" element={<Recipes />} />
+          <Route path="*" element={<PageNotFound />} />
         </Routes>
       </div>
       <div className="row">
---
+++ src/Containers/page-not-found/page-not-found.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import CHimage from "../../Assets/C&H_poised_downhill-image.png";
+import { HomeIcon } from "@primer/octicons-react";
+
+const pageNotFound = () => {
+  return (
+    <div>
+      <h1>
+        UH-OH ... Page not found!!!
+        <br />
+        <br />
+        <a href="/#">
+          <HomeIcon size={100} />
+        </a>
+        <br />
+        <br />
+        <img
+          src={CHimage}
+          alt="Calvin & Hobbs racing downhill in a red wagon"
+          width="1000"
+          height="600"
+        />
+      </h1>
+    </div>
+  );
+};
+
+export default pageNotFound;
