8281449b109c395dd5c44e4feae6b41108a3359e
Justin Sykes <atlsykes1@att.net>
2024-06-10 13:58:15 -0400

    updated image JASNASphoto img to be auto-size and be visibile on mobile apps, fixed vertical spacing in Navbar li elements to NOT impact rest of app

--- src/Components/navbar/navbar.jsx
+++ src/Components/navbar/navbar.jsx
@@ -5,7 +5,8 @@ import JASlogo from "../../Assets/Images/JAS-circ-logo-1.png";
 import { MailIcon } from "@primer/octicons-react";
 import { MarkGithubIcon } from "@primer/octicons-react";
 import LIlogo from "../../Assets/Images/In-Blue-48.png";
-//Navbar has to be integrated at the card level due to it's vertical nature
+//Navbar has to be integrated at the card level due to it's vertical nature. Bit of a hack to force some vertical spacing
+//in the Navbar links using a pre element and NOT impact other components or cards
 
 const navbar = () => {
   return (
@@ -46,6 +47,7 @@ const navbar = () => {
                   Recipes
                 </Link>
               </li>
+              <pre>{"\n"}</pre>
               <li>
                 <a
                   href="mailto: atlsykes1@att.net"
@@ -70,7 +72,11 @@ const navbar = () => {
                   target="_blank"
                   rel="noreferrer"
                 >
-                  <img src={LIlogo} alt="LinkedIn logo" />
+                  <img
+                    src={LIlogo}
+                    style={{ width: "50px", height: "50px" }}
+                    alt="LinkedIn logo"
+                  />
                 </a>
               </li>
             </ul>
